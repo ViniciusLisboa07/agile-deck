83c4cf5ae89aecacccfabf015b5f3d6cd3fa22d6
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2024-12-24 15:22:50 -0300

    feat: update home page layout and add user-specific room display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
+      @rooms = current_user.rooms
       @welcome_message = "Bem-vindo, #{current_user.email}!"
     else
       @welcome_message = "Bem-vindo ao Agile Deck!"
