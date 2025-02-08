7d74a466a8cbbaed1f82e12cb614baff6493bd77
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-02-08 01:15:06 -0300

    fix: adjust user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
-  skip_before_action :ensure_authentication, only: [ :choose_authentication, :anonymous_login ]
+  skip_before_action :ensure_authentication, only: [ :choose_authentication, :new_anonymous, :anonymous_login ]
 
   def choose_authentication
   end
 
+  def new_anonymous
+  end
+
   def anonymous_login
-    user = User.create_anonymous_user
+    user = User.create_anonymous_user(params[:name])
     sign_in(user)
     session[:anonymous_user] = user.id
     redirect_to home_path, notice: "Você está logado anonimamente."
+  end
+  
+  private
+  
+  def user_params
+    params.permit(:name)
   end
 end
