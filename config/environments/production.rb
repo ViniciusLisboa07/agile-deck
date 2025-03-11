2acc2ed51cdc2dabfee02b79892b9c47a1e02fb9
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-03-10 22:05:23 -0300

    chore: update production host configuration for Fly.io

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,8 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "example.com",     # Allow requests from example.com
-    "agile-deck.fly.dev"
-  ]
-  #
+  config.hosts << "agile-deck.fly.dev"
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
