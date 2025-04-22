e0e3aa4e62709686e0bf000258fda624643277b2
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-04-21 23:24:49 -0300

    Revert "chore: precompile assets for production deployment"
    This reverts commit 268f2047785ef58a565de34f2fd8c697da7655e1.

--- public/assets/controllers/index-1d7a1c01.js
+++
@@ -1,5 +0,0 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import "channels/room_channel"
-eagerLoadControllersFrom("controllers", application)
